41fb48a6aaab6d42674f1b0e1a46bdc3823c8ebc
Pantakarn Kowdokrak <peachoenixz@gmail.com>
2023-01-14 15:20:56 +0700

    fix err create

--- cloud_pocket/init.go
+++ cloud_pocket/init.go
@@ -5,6 +5,21 @@ import (
 	"github.com/kkgo-software-engineering/workshop/config"
 )
 
+type ResponseCloudPockets struct {
+	ID       int64   `json:"id"`
+	Name     string  `json:"name"`
+	Category string  `json:"category"`
+	Currency string  `json:"currency"`
+	Balance  float64 `json:"balance"`
+}
+
+type RequestCloudPockets struct {
+	Name           string  `json:"name"`
+	Currency       string  `json:"currency"`
+	InitialBalance float64 `json:"initial_balance"`
+	Category       string  `json:"category"`
+}
+
 type handler struct {
 	cfg config.FeatureFlag
 	db  *sql.DB
