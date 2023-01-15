e0cfef462f0a5ddc699aec54bcb840b20b77b586
Thuntita Kongiamtrakun <thuntita@Thuntitas-MacBook-Pro-14.local>
2023-01-15 09:39:09 +0700

    init transaction table

--- cloud_pocket/transfer.go
+++ cloud_pocket/transfer.go
@@ -9,10 +9,10 @@ import (
 )
 
 const (
-	balanceStmt = "SELECT balance from cloud_pockets WHERE id = $1;"
-	sStmt       = "UPDATE cloud_pockets SET balance = (balance - $2) WHERE id = $1 RETURNING id;"
-	dStmt       = "UPDATE cloud_pockets SET balance = (balance + $2) WHERE id = $1 RETURNING id;"
-	historyStmt = "INSERT INTO transactions (source_cloud_pocket_id, destination_cloud_pocket_id, amount, description, status) VALUES ($1, $2, $3, $4, $5) RETURNING transaction_id;"
+	bStmt = "SELECT balance from cloud_pockets WHERE id = $1;"
+	sStmt = "UPDATE cloud_pockets SET balance = (balance - $2) WHERE id = $1 RETURNING id;"
+	dStmt = "UPDATE cloud_pockets SET balance = (balance + $2) WHERE id = $1 RETURNING id;"
+	hStmt = "INSERT INTO transactions (source_cloud_pocket_id, destination_cloud_pocket_id, amount, description, status) VALUES ($1, $2, $3, $4, $5) RETURNING transaction_id;"
 )
 
 var (
@@ -31,7 +31,7 @@ func (h handler) Transfer(c echo.Context) error {
 	}
 
 	var balance float64
-	err = h.db.QueryRowContext(ctx, balanceStmt, t.SourceCloudPocketID).Scan(&balance)
+	err = h.db.QueryRowContext(ctx, bStmt, t.SourceCloudPocketID).Scan(&balance)
 	if err != nil {
 		logger.Error("query row error", zap.Error(err))
 		return err
@@ -45,21 +45,21 @@ func (h handler) Transfer(c echo.Context) error {
 	var sourcePocketId int64
 	err = h.db.QueryRowContext(ctx, sStmt, t.SourceCloudPocketID, t.Amount).Scan(&sourcePocketId)
 	if err != nil {
-		logger.Error("query row error", zap.Error(err))
+		logger.Error("update error", zap.Error(err))
 		return err
 	}
 
 	var destinationPocketId int64
 	err = h.db.QueryRowContext(ctx, dStmt, t.DestinationCloudPocketID, t.Amount).Scan(&destinationPocketId)
 	if err != nil {
-		logger.Error("query row error", zap.Error(err))
+		logger.Error("update error", zap.Error(err))
 		return err
 	}
 
 	var lastInsertId int64
-	err = h.db.QueryRowContext(ctx, historyStmt, t.SourceCloudPocketID, t.DestinationCloudPocketID, t.Amount, t.Desciption, "Success").Scan(&lastInsertId)
+	err = h.db.QueryRowContext(ctx, hStmt, t.SourceCloudPocketID, t.DestinationCloudPocketID, t.Amount, t.Desciption, "Success").Scan(&lastInsertId)
 	if err != nil {
-		logger.Error("query row error", zap.Error(err))
+		logger.Error("log error", zap.Error(err))
 		return err
 	}
 
--- db/01-init-transaction.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS "transactions" (
-    "transaction_id" SERIAL PRIMARY KEY,
-    "source_cloud_pocket_id" int4 NOT NULL,
-    "destination_cloud_pocket_id" int4 NOT NULL,
-    "amount" float8 NOT NULL,
-    "description" text,
-    "status" text,
-    "created_at" timestamp DEFAULT now()
-);
