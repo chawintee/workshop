1bd577292220ed40ab6e10ff1aca49da49df1e5d
Thuntita Kongiamtrakun <thuntita@Thuntitas-MacBook-Pro-14.local>
2023-01-15 13:02:30 +0700

    transfer function

--- cloud_pocket/transfer.go
+++ cloud_pocket/transfer.go
@@ -10,14 +10,14 @@ import (
 
 const (
 	bStmt = "SELECT balance from cloud_pockets WHERE id = $1;"
-	sStmt = "UPDATE cloud_pockets SET balance = (balance - $2) WHERE id = $1 RETURNING id;"
-	dStmt = "UPDATE cloud_pockets SET balance = (balance + $2) WHERE id = $1 RETURNING id;"
-	hStmt = "INSERT INTO transactions (source_cloud_pocket_id, destination_cloud_pocket_id, amount, description, status) VALUES ($1, $2, $3, $4, $5) RETURNING transaction_id;"
+	sStmt = "UPDATE cloud_pockets SET balance = (balance - $2) WHERE id = $1"
+	dStmt = "UPDATE cloud_pockets SET balance = (balance + $2) WHERE id = $1"
+	hStmt = "INSERT INTO transactions (source_cloud_pocket_id, destination_cloud_pocket_id, amount, description, status) VALUES ($1, $2, $3, $4, $5) RETURNING transaction_id, status;"
 )
 
 var (
 	hErrNotEnoughBalance = echo.NewHTTPError(http.StatusBadRequest,
-		"Not enough balance in the source cloud pocket")
+		TransactionErr{ErrorMessage: "Not enough balance in the source cloud pocket", Status: "Failed"})
 )
 
 func (h handler) Transfer(c echo.Context) error {
@@ -25,6 +25,7 @@ func (h handler) Transfer(c echo.Context) error {
 	ctx := c.Request().Context()
 	var t Transaction
 	err := c.Bind(&t)
+
 	if err != nil {
 		logger.Error("bad request body", zap.Error(err))
 		return echo.NewHTTPError(http.StatusBadRequest, "bad request body", err.Error())
@@ -42,24 +43,47 @@ func (h handler) Transfer(c echo.Context) error {
 		return hErrNotEnoughBalance
 	}
 
-	var sourcePocketId int64
-	err = h.db.QueryRowContext(ctx, sStmt, t.SourceCloudPocketID, t.Amount).Scan(&sourcePocketId)
+	// Get a Tx for making transaction requests.
+	tx, err := h.db.BeginTx(ctx, nil)
 	if err != nil {
-		logger.Error("update error", zap.Error(err))
+		logger.Error("error", zap.Error(err))
 		return err
 	}
-
-	var destinationPocketId int64
-	err = h.db.QueryRowContext(ctx, dStmt, t.DestinationCloudPocketID, t.Amount).Scan(&destinationPocketId)
-	if err != nil {
-		logger.Error("update error", zap.Error(err))
-		return err
-	}
+	// Defer a rollback in case anything fails.
+	defer tx.Rollback()
 
 	var lastInsertId int64
-	err = h.db.QueryRowContext(ctx, hStmt, t.SourceCloudPocketID, t.DestinationCloudPocketID, t.Amount, t.Desciption, "Success").Scan(&lastInsertId)
+	_, err = tx.ExecContext(ctx, sStmt, t.SourceCloudPocketID, t.Amount)
+	if err != nil {
+		logErr := tx.QueryRowContext(ctx, hStmt, t.SourceCloudPocketID, t.DestinationCloudPocketID, t.Amount, t.Desciption, "Failed").Scan(&lastInsertId, &t.Status)
+		if logErr != nil {
+			logger.Error("log error", zap.Error(logErr))
+			return logErr
+		}
+		t.TransactionID = lastInsertId
+		return c.JSON(http.StatusInternalServerError, TransactionErr{ErrorMessage: "update err", Status: t.Status})
+	}
+
+	_, err = tx.ExecContext(ctx, dStmt, t.DestinationCloudPocketID, t.Amount)
+	if err != nil {
+		logErr := tx.QueryRowContext(ctx, hStmt, t.SourceCloudPocketID, t.DestinationCloudPocketID, t.Amount, t.Desciption, "Failed").Scan(&lastInsertId, &t.Status)
+		if logErr != nil {
+			logger.Error("log error", zap.Error(logErr))
+			return logErr
+		}
+		t.TransactionID = lastInsertId
+		return c.JSON(http.StatusInternalServerError, TransactionErr{ErrorMessage: "update err", Status: t.Status})
+	}
+
+	err = tx.QueryRowContext(ctx, hStmt, t.SourceCloudPocketID, t.DestinationCloudPocketID, t.Amount, t.Desciption, "Success").Scan(&lastInsertId, &t.Status)
 	if err != nil {
 		logger.Error("log error", zap.Error(err))
+		return err
+	}
+
+	// Commit the transaction.
+	if err = tx.Commit(); err != nil {
+		logger.Error("error", zap.Error(err))
 		return err
 	}
 
